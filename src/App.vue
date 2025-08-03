9c18af5acb0cba5aa7402a603bd0cf6421207ffd
christopher-k-c <chriscareyk@gmail.com>
2025-08-03 13:55:28 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+
+import Header from './components/Header.vue';
+
+import { ref } from 'vue';
+
+const activeTab = ['interactions']
+
+
+
+
+
+</script>
+
+<template>
+
+  <Header>
+
+  </Header>
+
+
+
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<script setup>
+
+
+
+
+
+</script>
+
+<template>
+
+<div class="topbar">
+  <!-- Tab Buttons -->
+  <nav class="tab-buttons">
+    <button class="bartab">Orca Interactions</button>
+    <button class="bartab">Uneventful Passages</button>
+    <button class="bartab">Comparative Data</button>
+  </nav>
+</div>
+
+  <!-- Search Field -->
+  <div class="search-controls">
+    <div class="date-inputs">
+      <input type="date" name="" id="">
+    </div>
+    <div class="search-bar"></div>
+  </div>
+
+
+
+
+</template>
+
+<style scoped>
+
+.topbar {
+  border-bottom: 2px solid #003366;
+  margin-bottom: 10px;
+  padding-bottom: -8px;
+}
+.tabbar {
+  display: inline-block;
+  height: 35px;
+  padding: 5px;
+  font-weight: bold;
+}
+.bartab {
+  display: inline-block;
+  margin: 0px 6px 0px 6px;
+  border: 2px solid #003366;
+  border-bottom: 0;
+  border-radius: 6px 6px 0 0;
+  cursor: pointer;
+  padding: 0 4px 0 4px;
+  background: #ffffff;
+}
+
+
+</style>
